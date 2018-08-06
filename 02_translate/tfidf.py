9ad4b09516e450956b7a56f9cce03e58ff30f2e5
Eduardo Dutra <eduardo.dutra@habibs.com.br>
2018-08-06 15:11:14 -0300

    frequency graph

--- 02_translate/tfidf.py
+++ 02_translate/tfidf.py
@@ -28,11 +28,15 @@ import matplotlib.pyplot as plt
 # Define limits for negative and positive sentiment
 
 NEGATIVE_START = .0
-NEGATIVE_END = .3
+NEGATIVE_END = .25
 
-POSITIVE_START = .7
+POSITIVE_START = .75
 POSITIVE_END = 1.
 
+
+# exclude the tweets with the following terms
+exclude_terms = [r'\bcuando\b', r'\by\b', r'\ben\b', r'\bla\b', r'\blas\b', r'\bpaula amoedo\b', r'\bsporting\b', r'\blo\b', r'\byo\b']
+re_exclude = re.compile(r'|'.join(exclude_terms), re.IGNORECASE)
 
 candidates = [{'candidate' : candidato.split('_')[1].split('.')[0], 'filename' : candidato, 'type': candidato.split('.')[3]} for candidato in glob('../data/sentiment/sentimento*.csv')]
 
@@ -42,11 +46,11 @@ for candidate in candidates:
         with open(candidate['filename'], newline='\r\n', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile, delimiter=';', quotechar='"')
             data = list(reader)
-            candidate['data'] = [{'full_text' : tweet[1], 'score' : (float(tweet[7])+1)/2 } for tweet in data]
+            candidate['data'] = [{'full_text' : tweet[1], 'score' : (float(tweet[7])+1)/2 } for tweet in data if not re.search(re_exclude, tweet[1])]
     else:
         with open(candidate['filename']) as f:
             data = json.load(f)
-            candidate['data'] = [{'full_text' : tweet['full_text'], 'score' : tweet['score']} for tweet in data]
+            candidate['data'] = [{'full_text' : tweet['full_text'], 'score' : tweet['score']} for tweet in data if not re.search(re_exclude, tweet['full_text'])]
 
 # Clear text
             
@@ -66,47 +70,58 @@ for candidate in candidates:
 # Join texts by sentiment
 
 for candidate in candidates:
-    candidate['text_positive'] = '\n'.join( [tweet['full_text'].lower() for tweet in candidate['data'] if tweet['score'] >= POSITIVE_START and tweet['score'] <= POSITIVE_END ] )
-    candidate['text_negative'] = '\n'.join( [tweet['full_text'].lower() for tweet in candidate['data'] if tweet['score'] >= NEGATIVE_START and tweet['score'] <= NEGATIVE_END ] )
-    candidate['text'] = '\n'.join( [tweet['full_text'].lower() for tweet in candidate['data']] )
+    candidate['text'] = {}
+    candidate['text']['positive'] = '\n'.join( [tweet['full_text'].lower() for tweet in candidate['data'] if tweet['score'] >= POSITIVE_START and tweet['score'] <= POSITIVE_END ] )
+    candidate['text']['negative'] = '\n'.join( [tweet['full_text'].lower() for tweet in candidate['data'] if tweet['score'] >= NEGATIVE_START and tweet['score'] <= NEGATIVE_END ] )
+    candidate['text']['neutral'] = '\n'.join( [tweet['full_text'].lower() for tweet in candidate['data']] )
+    #candidate['text_positive'] = '\n'.join( [tweet['full_text'].lower() for tweet in candidate['data'] if tweet['score'] >= POSITIVE_START and tweet['score'] <= POSITIVE_END ] )
+    #candidate['text_negative'] = '\n'.join( [tweet['full_text'].lower() for tweet in candidate['data'] if tweet['score'] >= NEGATIVE_START and tweet['score'] <= NEGATIVE_END ] )
+    #candidate['text'] = '\n'.join( [tweet['full_text'].lower() for tweet in candidate['data']] )
+
+sentiments = ['neutral', 'positive', 'negative']
+
+    
+textlist = {}
+for sentiment in sentiments:
+    textlist[sentiment] = [{'candidate': c['candidate'], 'text': c['text'][sentiment]} for c in candidates]
 
 
 
-textlist = [{'candidate': c['candidate'], 'text': c['text']} for c in candidates]
+sentiment_dataframes = {}
+for sentiment in sentiments:
+    wordlist = []
+    print(sentiment)
+    for text in textlist[sentiment]:
+        print('\t{}: {}'.format(text['candidate'], len(text['text'])))
+        tmp = pd.DataFrame(word_tokenize(text['text']), columns=['word'])
+        tmp['candidate'] = text['candidate']
+        wordlist.append( tmp.groupby(['candidate', 'word']).size().to_frame('frequency').reset_index() )
+    tmp_df = pd.concat(wordlist)
+    tmp_df.index = np.arange(tmp_df.shape[0])
+    sentiment_dataframes[sentiment] = tmp_df
 
 
-
-"""freqlist = []
-for item in textlist:
-    freq = WordCloud(collocations=False, stopwords=stopwords_pt).process_text(item['text'])
-    freqlist =  freqlist + [(item['candidate'], f, freq[f])  for f in freq]
-"""
-
-wordlist = []
-for text in textlist:
-    tmp = pd.DataFrame(word_tokenize(text['text']), columns=['word'])
-    tmp['candidate'] = text['candidate']
-    wordlist.append( tmp.groupby(['candidate', 'word']).size().to_frame('frequency').reset_index() )
-
-df = pd.concat(wordlist)
-
-
-df.index = np.arange(df.shape[0])
-
-df['rank'] = df.groupby(['candidate'])['frequency'].rank(ascending=False)
-df['total_words'] = df.groupby(['candidate']).frequency.transform(np.sum)
-df['n_containing'] = df.groupby(['word']).candidate.transform(np.size)
-df['tf'] = df['frequency']/df.total_words 
-df['idf'] = np.log( 8 / df.n_containing )
-df['tfidf'] = df.tf*df.idf
+for item in sentiment_dataframes.items():  
+    print(item[0])
+    df = item[1]
+    df['total_words'] = df.groupby(['candidate']).frequency.transform(np.sum)
+    df['n_containing'] = df.groupby(['word']).candidate.transform(np.size)
+    df['tf'] = df['frequency']/df.total_words 
+    df['idf'] = np.log( len(candidates) / df.n_containing )
+    df['tfidf'] = df.tf*df.idf
+    df = df[df['tfidf'] != 0]
+    
+#for item in sentiment_dataframes.items():  
+#    print(item[0])
+#    df = item[1]
+#    df['rank'] = df.groupby(['candidate'])['frequency'].rank(ascending=False)
 
 
-
-amoedo = df[(df['candidate'] == 'amoedo') &(df['rank'] > 0) & (df['tfidf'] != 0)].sort_values([ 'tfidf', 'candidate'], ascending=[False, True]).head(30)
+#amoedo = df[(df['candidate'] == 'amoedo') &(df['rank'] > 0) & (df['tfidf'] != 0)].sort_values([ 'tfidf', 'candidate'], ascending=[False, True]).head(30)
 
 
 #df['rank'] = 
-df.groupby(['candidate'])['frequency'].rank(ascending=False)
+#df.groupby(['candidate'])['frequency'].rank(ascending=False)
 
 #df.index = np.arange(df.shape[0])
 
@@ -122,21 +137,28 @@ def to_multidict(items):
 #    print('{}: {}'.format(item[0], item[1]))
 
 brazil_mask = np.array(Image.open("brazil_mask.png"))
+for item in sentiment_dataframes.items():  
+    print(item[0])
+    df = item[1]
+    for start in list(range(0,0+1,5)):
+        for candidate in candidates:
+            print(candidate['candidate'])
+            freqs = df[(df['candidate'] == candidate['candidate']) & (df['rank'] > start) & (df['tfidf'] != 0)][['word', 'frequency']]
+            
+            df['rank'] = df[df['tfidf'] != 0].groupby(['candidate'])['frequency'].rank(ascending=False)
+            df_plt = df[(df['candidate'] == candidate['candidate']) & (df['rank'] > start) & (df['tfidf'] != 0)].sort_values([ 'candidate', 'frequency'], ascending=[True, False])[['candidate', 'word', 'frequency']].head(15)
+            ax = df_plt.plot.barh(x='word', y='frequency', title='Top 15 {} words for {}'.format(item[0], candidate['candidate'].upper()), figsize=(12,8))
+            ax.invert_yaxis()
+            fig = ax.get_figure()
+            fig.savefig('../imagens/barh_tfidf_{}_{}_start_{}.png'.format(candidate['candidate'], item[0], start))
 
-for candidate in candidates:
-    print(candidate['candidate'])
-    freqs = df[(df['candidate'] == candidate['candidate']) & (df['rank'] > 0) & (df['tfidf'] != 0)][['word', 'frequency']]
-    wordcloud = WordCloud(width=1200, height=1200, background_color="white", mask=brazil_mask,normalize_plurals=False)#,
-               #stopwords=stopwords_pt, font_path='/Library/Fonts/Times New Roman.ttf')
-    wordcloud.generate_from_frequencies(to_multidict(freqs.values))
-    plt.axis("off")
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.show()
-    break
+           #freqs = df[(df['candidate'] == 'amoedo') & (df['rank'] > 0) & (df['tfidf'] != 0)][['word', 'frequency']]
+            wordcloud = WordCloud(width=1200, height=1200, background_color="white", mask=brazil_mask,normalize_plurals=False)#,
+                       #stopwords=stopwords_pt, font_path='/Library/Fonts/Times New Roman.ttf')
+            wordcloud.generate_from_frequencies(to_multidict(freqs.values))
+            plt.axis("off")
+            plt.imshow(wordcloud, interpolation="bilinear")
+            plt.show()
+            wordcloud.to_file('../imagens/tfidf_{}_{}_start_{}.png'.format(candidate['candidate'], item[0], start))
 
-freqs.head(5).values
-
-frequencies = [{x[0] : x[1]} for x in freqs.values]
-sorted(frequencies.items(), key=itemgetter(1), reverse=True)
-wordcloud.fit_words(frequencies)
 
